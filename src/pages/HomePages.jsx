89a01945e4661b0fcd720565fef1cfc1c615e646
ilnaz valeev <alexsanderkyznecov@mail.ru>
2025-07-20 21:05:19 +0500

    сдеалал главнуб страницу, переделал названия классов, начал делать страницу фильма

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,54 +1,111 @@
-//Header.jsx
-
-import React, {useState} from 'react';
+// HomePage.jsx
+import React from 'react';
 import "../css/HomePages.min.css";
 
-
-const HomePages = ({onSearch}) => {
-
-
+const HomePage = ({ onSearch }) => {
     return (
-      <section className="Home-Pages">
-        <section className="header">
-          {/* Секция для пользователя */}
-          <section className="header__user">
-            <h1 className="header__greeting">Hi, Name!</h1>
-            <img
-              className="header__photo"
-              src="https://live.staticflickr.com/65535/52879735901_205eff2209_h.jpg"
-              alt="Фото профиля"
-            />
-          </section>
+        <main className="home-page">
+            {/* Header Section */}
+            <header className="page-header">
+                {/* User Profile Section */}
+                <div className="user-profile">
+                    <h1 className="user-profile__greeting">Hi, Name!</h1>
+                    <img
+                        className="user-profile__avatar"
+                        src="https://live.staticflickr.com/65535/52879735901_205eff2209_h.jpg"
+                        alt="User profile"
+                    />
+                </div>
 
-          {/* Поиск */}
-          <section className="header__search">
-            <img src="#" alt="" />
-            <input
-              type="text"
-              //   value={searchQuery}
-              //   onChange={handleSearchChange}
-              placeholder="Search"
-              className="input-search"
-            />
-          </section>
-        </section>
-        <section>
-          <section className="for-yuo">
-            <h2>For you</h2>
-            <div className="film_container"></div>
+                {/* Search Section */}
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="search-container__input"
+                    />
+                </div>
+            </header>
 
-            <div className="paganation">
-              <span className="paganation__point"></span>
-              <span className="paganation__point"></span>
-              <span className="paganation__point"></span>
-              <span className="paganation__point"></span>
-            </div>
-          </section>
-        </section>
+            {/* Recommendations Section */}
+            <section className="recommendations-section">
+                <h2 className="section-title">For you</h2>
+                <div className="movie-carousel"></div>
+                <div className="carousel-indicators">
+                    {[1, 2, 3, 4].map((item) => (
+                        <span key={item} className="carousel-indicators__dot"></span>
+                    ))}
+                </div>
+            </section>
 
-      </section>
+            {/* Popular Movies Section */}
+            <section className="movie-section">
+                <div className="section-header">
+                    <h3 className="section-header__title">Popular</h3>
+                    <span className="section-header__action">See all</span>
+                </div>
+                <div className="movie-grid">
+                    {[1, 2].map((item) => (
+                        <div key={item} className="movie-card">
+                            <div className="movie-card__poster"></div>
+                            <div className="movie-card__info">
+                                <span className="movie-card__title">Alien</span>
+                                <span className="movie-card__rating">
+                                    8.2 <span className="movie-card__star-icon"></span>
+                                </span>
+                                <span className="movie-card__year">2021</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+            {/* Genres Section */}
+            <section className="movie-section">
+                <div className="section-header">
+                    <h3 className="section-header__title">Genres</h3>
+                    <span className="section-header__action">See all</span>
+                </div>
+                <div className="genre-grid">
+                    <div className="genre-card">
+                        <div className="genre-card__image"></div>
+                        <span className="genre-card__name">Horror</span>
+                    </div>
+                </div>
+            </section>
+
+            {/* Legendary Movies Section */}
+            <section className="movie-section">
+                <div className="section-header">
+                    <h3 className="section-header__title">Legendary movies</h3>
+                    <span className="section-header__action">See all</span>
+                    
+                </div>
+                <div className="movie-grid">
+                    {[1, 2].map((item) => (
+                        <div key={item} className="movie-card">
+                            <div className="movie-card__poster"></div>
+                            <div className="movie-card__info">
+                                <span className="movie-card__title">Alien</span>
+                                <span className="movie-card__rating">
+                                    8.2 <span className="movie-card__star-icon"></span>
+                                </span>
+                                <span className="movie-card__year">2021</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+            {/* Bottom Navigation */}
+            <nav className="bottom-navigation">
+                <button className="bottom-navigation__button bottom-navigation__button--home">  </button>
+                <button className="bottom-navigation__button bottom-navigation__button--discover">ico</button>
+                <button className="bottom-navigation__button bottom-navigation__button--movies">ico</button>
+                <button className="bottom-navigation__button bottom-navigation__button--profile">ico</button>
+            </nav>
+        </main>
     );
-}
+};
 
-
-export default HomePages;
+export default HomePage;
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -7,7 +7,42 @@ import "../css/MovieDeatailsPage.min.css";
 const MovieDetailsPage = ({ onSearch }) => {
   return(
     <section className="Movie__details-page">
+        <section className='mov__deatails-img'>
 
+        </section>
+        <section className='movies__text'>
+            <span className='movies__name'>
+                Dune
+            </span>
+            <span className='movies__rainteng'>
+                8.2 
+            </span>
+            <span className='movies__name'>
+                <span>
+                    2021
+                </span>
+                <span>
+                    Denis Villeneuve
+                </span>
+            </span>
+            <span className='movies__genres'>
+                <span>
+                    Epic
+                </span>
+                <span>
+                    Fantasy
+                </span>
+                <span>
+                    Drama
+                </span>
+            </span>
+            <span className='movies__text'>
+                Lorem ipsum menpsum, quibusdam sit! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, pariatur. Lorem ipsum dolor sit amet.
+            </span>
+            <span>
+                Cast
+            </span>
+        </section>
   </section>
   ) 
 };
